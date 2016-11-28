d07f0fd6768544d68d788073db11fbf92cf92e24
Alex M <Alex@Williams-MBP.fios-router.home>
2016-11-28 15:33:19 -0500

    finished number 8

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 function yearOfBirth(age) {
-	var yearOfBirth = 2016 - age;
-	return yearOfBirth;
+	if (age < 0) {
+		throw new Error("Age can not be negative");
+	}
+	return 2016 - age;
 }
 
+
 function whoAmI (name, age) {
+	if (typeof name !== "string" || typeof age !== "number") {
+		console.error("Arguments not valid")
+	}
+
+	else {
 	var yob = yearOfBirth(age);
 	console.log("'" + "Hi, my name is " + name + " and I'm " + age + " years old" + "'")
 	console.log("I was born in " + yob + ".")
+	}
 }
 
-
-
-whoAmI("Alex", 29);
+whoAmI("Chris", -5);
