0e622bea3f20acffba0e879e10b9f691d4780fea
Alex M <Alex@Williams-MBP.fios-router.home>
2016-11-28 14:31:43 -0500

    added new function

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function whoAmI (name, age) {
+function yearOfBirth(age) {
 	var yearOfBirth = 2016 - age;
-	console.log("'" + "Hi, my name is " + name + " and I'm " + age + " years old" + "'")
-	console.log("I was born in " + yearOfBirth + ".")
+	return yearOfBirth;
 }
+
+function whoAmI (name, age) {
+	var yob = yearOfBirth(age);
+	console.log("'" + "Hi, my name is " + name + " and I'm " + age + " years old" + "'")
+	console.log("I was born in " + yob + ".")
+}
+
+
 
 whoAmI("Alex", 29);
